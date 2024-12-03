d718dde7190c91e6a8940139cb878b42b1058018
timepresent95 <timepresent95@gmail.com>
2024-12-03 20:34:06 +0900

    Feat: ELB 모듈 구축

---
+++ main.tf
@@ -0,0 +1,179 @@
+/*
+  보안 그룹
+*/
+
+module "security_group" {
+  source  = "app.terraform.io/animal-squad/security-group/aws"
+  version = "1.0.0"
+
+  name_prefix = "${var.name}-alb-sg"
+  vpc_id      = var.vpc_id
+
+  #map(object({ from_port = number to_port = number ip_protocol = string cidr_ipv4 = optional(string, "0.0.0.0/0") ref_sg_id = optional(string, null) }))
+  ingress_rules {
+    http = {
+      from_port   = 80
+      to_port     = 80
+      ip_protocol = "tcp"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+    https = {
+      from_port   = 443
+      to_port     = 443
+      ip_protocol = "tcp"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+  }
+}
+
+/*
+  ALB
+*/
+
+resource "aws_lb" "alb" {
+  name               = var.name
+  load_balancer_type = "application"
+
+  subnets         = var.subnet_ids
+  security_groups = [aws_security_group.sg.id]
+
+  client_keep_alive = 3600 // Default
+  idle_timeout      = var.idle_timeout
+
+  enable_deletion_protection = var.enable_deletion_protection
+}
+
+/*
+  ALB Listener
+*/
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.alb.arn
+
+  port     = "80"
+  protocol = "HTTP"
+
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.alb.arn
+  certificate_arn   = var.certificate_arn
+
+  //NOTE: ssl_policy 설정 값 참조 https://docs.aws.amazon.com/elasticloadbalancing/latest/application/describe-ssl-policies.html
+  ssl_policy = "ELBSecurityPolicy-TLS13-1-0-2021-06"
+  port       = "443"
+  protocol   = "HTTPS"
+
+
+  default_action {
+    type = "forward"
+
+    forward {
+      dynamic "target_group" {
+        for_each = aws_lb_target_group.default_target_groups
+
+        content {
+          arn = target_group.value.arn
+        }
+      }
+    }
+  }
+}
+
+resource "aws_lb_target_group" "default_target_groups" {
+  for_each = var.default_target_groups
+
+  vpc_id                            = var.vpc_id
+  load_balancing_cross_zone_enabled = "use_load_balancer_configuration"
+  target_type                       = "instance"
+
+  name = each.key
+  port = each.value.port
+
+  protocol         = "HTTP"
+  protocol_version = "HTTP1"
+
+  health_check {
+    path = each.value.health_check_path
+  }
+
+  tags = {
+    Name = "${var.name}-default-tg-${each.key}"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "default_target" {
+  for_each = var.default_targets
+
+  target_group_arn = aws_lb_target_group.default_target_groups[each.value.target_group_key].arn
+  target_id        = each.key
+  port             = each.value.port
+}
+
+/*
+  ALB Listener Rule
+*/
+
+resource "aws_lb_listener_rule" "https_listener_rules" {
+  for_each = var.https_listener_rules
+
+  listener_arn = aws_lb_listener.https.arn
+  priority     = each.value.priority
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_groups[each.key].arn
+  }
+
+  condition {
+    path_pattern {
+      values = each.value.path
+    }
+  }
+
+  condition {
+    host_header {
+      values = each.value.host
+    }
+  }
+}
+
+resource "aws_lb_target_group" "target_groups" {
+  for_each = var.target_groups
+
+  vpc_id                            = var.vpc_id
+  load_balancing_cross_zone_enabled = "use_load_balancer_configuration"
+  target_type                       = "instance"
+
+  name = each.key
+  port = each.value.port
+
+  protocol         = "HTTP"
+  protocol_version = "HTTP1"
+
+  health_check {
+    path = each.value.health_check_path
+  }
+
+  tags = {
+    Name = "${var.name}-tg-${each.key}"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "target" {
+  for_each = var.targets
+
+  target_group_arn = aws_lb_target_group.target_groups[each.value.target_group_key].arn
+  target_id        = each.key
+  port             = each.value.port
+}
---
+++ variables.tf
@@ -0,0 +1,118 @@
+/*
+  공통 사항
+*/
+
+variable "name" {
+  description = "ALB 구성 요소들의 이름과 tag을 선언하는데 사용될 이름."
+  type        = string
+
+  validation {
+    condition     = length(var.name) <= 32
+    error_message = "이름은 32자를 넘을 수 없습니다."
+  }
+}
+
+/*
+  네트워크 및 보안 그룹
+*/
+
+variable "vpc_id" {
+  description = "ALB의 보안 그룹을 생성 할 vpc의 id"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "ALB가 관리하는 subnet id 목록. 필수적으로 2개 이상의 AZ를 포함"
+  type        = set(string)
+
+  validation {
+    condition     = length(var.subnet_ids) >= 2
+    error_message = "ALB는 최소한 2개 이상의 AZ를 포함하는 subnet을 가져야합니다."
+  }
+}
+
+variable "enable_deletion_protection" {
+  description = "ALB 삭제를 불가능하게 만들어 안전하게 관리할지 여부. true로 설정된 경우 false로 변경 후에 삭제 가능"
+  type        = bool
+  default     = false
+}
+
+variable "idle_timeout" {
+  description = "ALB에서 연결이 일정 시간동안 idle할 경우 자동으로 종료. health check 시간보다 짧을 경우 idle 상태로 판단하여 health check가 실패할 수 있습니다."
+  type        = number
+  default     = 60
+}
+
+variable "certificate_arn" {
+  description = "ALB에서 https를 인증 받을 때 사용할 인증서의 arn"
+  type        = string
+}
+
+/*
+  ALB 작동 방식
+*/
+
+variable "default_target_groups" {
+  description = "default target group 리스트. key 값은 32자 이하, 영어와 hyphen만 가능"
+
+  type = map(
+    object({
+      health_check_path = optional(string)
+      port              = number
+    })
+  )
+
+  validation {
+    condition     = length(var.default_target_groups) >= 1 && length(var.default_target_groups) <= 5
+    error_message = "alb의 default target group은 1개 이상 5개 이하만 가능합니다."
+  }
+}
+
+variable "default_targets" {
+  description = "default target group 별 target. key값을 통해 target id 지정"
+
+  type = map(
+    object({
+      target_group_key = string
+      port             = number
+    })
+  )
+}
+
+variable "https_listener_rules" {
+  description = "https 리스너에 추가로 연결 할 리스너 설정. priority는 1부터 50,000 사이의 값이며 중복이 있으면 안된다. key 값은 32자 이하, 영어와 hyphen만 가능"
+  type = map(object({
+    path              = list(string)
+    host              = list(string)
+    priority          = number
+    health_check_path = optional(string)
+    port              = number
+  }))
+}
+
+variable "target_groups" {
+  description = "listener의 key값을 통해 target group 리스트. key 값은 32자 이하, 영어와 hyphen만 가능"
+
+  type = map(
+    object({
+      health_check_path = optional(string)
+      port              = number
+    })
+  )
+
+  validation {
+    condition     = length(var.default_target_groups) >= 1 && length(var.default_target_groups) <= 5
+    error_message = "alb의 target group은 1개 이상 5개 이하만 가능합니다."
+  }
+}
+
+variable "targets" {
+  description = "target group의 key값을 통해 target id 지정"
+
+  type = map(
+    object({
+      target_group_key = string
+      port             = number
+    })
+  )
+}
